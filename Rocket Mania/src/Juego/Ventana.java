c2f0f5857cd3a87f3d15084a9a4486a1c65848c2
Alumno <Alumno@10.206.95.129>
2014-11-11 18:41:08 +0100

    Version 1.3 modificado 11/11 18:40

--- Rocket Mania/src/Juego/Ventana.java
+++ Rocket Mania/src/Juego/Ventana.java
@@ -100,6 +100,7 @@ public class Ventana extends JFrame implements  MouseListener {
 	public static void main(String[] args) {
 		Ventana v = new Ventana();
 		v.setVisible(true);
+		v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public void revArriba(){
